02b904e1c6d5eaa2ac50a08b48b410ac68453cfe
herocrab <jeremyleebuck@gmail.com>
2020-10-20 13:29:39 -0600

    Initial Commit.

---
+++ StrideOutlineRenderer/StrideOutlineRenderer.Windows/StrideOutlineRendererApp.cs
@@ -0,0 +1,15 @@
+using Stride.Engine;
+
+namespace StrideOutlineRenderer
+{
+    class StrideOutlineRendererApp
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
