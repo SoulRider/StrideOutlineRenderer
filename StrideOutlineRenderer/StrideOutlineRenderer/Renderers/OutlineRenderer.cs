b6e2ad518daf4ab73eadbf369f6bc42ea443c6d2
herocrab <jeremyleebuck@gmail.com>
2020-10-20 19:21:25 -0600

    Modified scale of outline.

--- StrideOutlineRenderer/StrideOutlineRenderer/Renderers/OutlineRenderer.cs
+++ StrideOutlineRenderer/StrideOutlineRenderer/Renderers/OutlineRenderer.cs
@@ -1,4 +1,4 @@
-﻿using Stride.Core;
+using Stride.Core;
 using Stride.Core.Mathematics;
 using Stride.Games;
 using Stride.Graphics;
@@ -15,39 +15,54 @@ namespace StrideOutlineRenderer.Renderers
 
 
         public Texture OutlineRenderTarget { get; set; }
-        public float Scale { get; set; } = 1.1f;
-        public Color Color { get; set; } = Color.Red;
+        public float Scale { get; set; } = 1.01f;
 
-        private SpriteBatch spriteBatch;
-        private Vector2 textureOffset;
+        public Color Color
+        {
+            get => _color;
+            set
+            {
+                _color = value;
+                _amplifiedColor = new Color4(
+                    Color.R * ColorAmplifier, 
+                    Color.G * ColorAmplifier,
+                    Color.B * ColorAmplifier, 
+                    ColorAmplifier);
+            }
+        }
 
-        private static readonly Color ColorMultiplier = new Color(10f, 10f, 10f);
+        private SpriteBatch _spriteBatch;
+        private Vector2 _textureOffset;
+
+        private Color _color;
+        private Color4 _amplifiedColor;
+
+        private const float ColorAmplifier = 10f;
 
         protected override void InitializeCore()
         {
             base.InitializeCore();
 
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            textureOffset = new Vector2((OutlineRenderTarget.Width - OutlineRenderTarget.Width * Scale) / 2,
+            _textureOffset = new Vector2((OutlineRenderTarget.Width - OutlineRenderTarget.Width * Scale) / 2,
                 (OutlineRenderTarget.Height - OutlineRenderTarget.Height * Scale) / 2);
         }
 
         protected override void DrawCore(RenderContext context, RenderDrawContext drawContext)
         {
-            spriteBatch.Begin(drawContext.GraphicsContext, depthStencilState: DepthStencilStates.None);
+            _spriteBatch.Begin(drawContext.GraphicsContext, depthStencilState: DepthStencilStates.None);
 
-            // TODO adjust for offset
-            spriteBatch.Draw(OutlineRenderTarget, textureOffset, Color, 0, Vector2.Zero,Scale);
-            //spriteBatch.Draw(OutlineRenderTarget, Vector2.Zero);
-            spriteBatch.End();
+            _spriteBatch.Draw(OutlineRenderTarget, _textureOffset, _amplifiedColor, 0, Vector2.Zero,Scale);
+            _spriteBatch.Draw(OutlineRenderTarget, Vector2.Zero);
+            _spriteBatch.End();
         }
 
         protected override void Destroy()
         {
             base.Destroy();
 
-            spriteBatch.Dispose();
+            _spriteBatch.Dispose();
         }
     }
 }
